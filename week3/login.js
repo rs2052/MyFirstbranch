d20dbcf7f465b6c4557fa4c46ffe1187b2d88b42
rs2052 <145095585+rs2052@users.noreply.github.com>
2023-10-23 10:52:45 +0400

    Create login.js

---
+++ week3/login.js
@@ -0,0 +1,105 @@
+
+const form = document.getElementById("form");
+form.addEventListener("submit", function (event) {
+
+   let valid = true;
+   const emptyFields = [];
+   const username = document.getElementById("LOGIN ID");
+   const usernameError = document.getElementById("ID ERROR");
+
+   if (username.value.trim() === "") {
+
+       emptyFields.push("LOGIN ID");
+       usernameError.textContent = "LOGIN ID MISSING";
+       username.classList.remove("VALID ID");
+       valid = false;
+
+} else {
+  
+   usernameError.textContent = "";
+   username.classList.add("VALIC ID");
+
+}
+
+
+const email = document.getElementById("EMAIL");
+
+const emailError = document.getElementById("EMAIL VERIFICATION FAILED");
+
+const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+if (!emailPattern.test(email.value)) {
+  
+   emailError.textContent = "INCORRECT EMAIL ID";
+  
+   email.classList.remove("VALID");
+ 
+   valid = false;
+
+} else {
+  
+   emailError.textContent = "";
+  
+   email.classList.add("VALID");
+
+}
+
+
+// Password validation
+
+const password = document.getElementById("PASSCODE");
+
+const passwordError = document.getElementById("PASSCODE");
+
+if (password.value.length < 8) {
+
+   passwordError.textContent = "PASSWORD MUST BE ATLEAST 8 CHARECTERS";
+
+   password.classList.remove("VALID");
+
+   valid = false;
+
+} else {
+
+   passwordError.textContent = "";
+
+   password.classList.add("VALID");
+
+}
+
+
+// Confirm Password validation
+
+const confirmPassword = document.getElementById("confirmPassword");
+
+const confirmPasswordError = document.getElementById("confirmPasswordError");
+
+if (confirmPassword.value !== password.value) {
+
+   confirmPasswordError.textContent = "Passwords do not match";
+
+   confirmPassword.classList.remove("valid");
+
+   valid = false;
+
+} else {
+
+   confirmPasswordError.textContent = "";
+
+   confirmPassword.classList.add("valid");
+
+}
+
+if (emptyFields.length > 0) {
+
+   alert("The following fields are required: " + emptyFields.join(", "));
+ 
+   valid = false;
+
+}
+
+if (!valid) {
+
+   event.preventDefault(); 
+   
+}});
