8c850eff5895e6c600b1e113b1a3cfb951eb08b7
rs2052 <145095585+rs2052@users.noreply.github.com>
2023-10-23 10:05:11 +0400

    Update script.js

--- week3/script.js
+++ week3/script.js
@@ -1,74 +1,25 @@
-
-let slideIndex = 1;
-
-// Call the showSlides function to display the initial slide
-
-showSlides(slideIndex);
-
-// Function to navigate to the next or previous slide
-
+let slideN = 1;
+show(slideN);
 function plusSlides(n) {
-  
- // Call showSlides with the updated slide index
-
-    showSlides(slideIndex += n);
-
+    show(slideN += n);
 }
-
-// Function to navigate to a specific slide
-
 function currentSlide(n) {
-  
- // Set the slide index to the specified value and call showSlides
-
-    showSlides(slideIndex = i);
-
+    show(slideN = i);
 }
 
-// Function to control the display of slides
-
-function showSlides(n) {
-  
+function show(n) {
     let i;
-  
- // Get all elements with the class "mySlides" (representing slides)
-
-    let slides = document.getElementsByClassName("mySlides");
-  
- // Get all elements with the class "dot" (representing navigation dots)
-
-    let dots = document.getElementsByClassName("dot");
-
-// If the slide index is greater than the number of slides, set it to 1
-  
-    if (n > slides.length) {slideIndex = 1}
-
-// If the slide index is less than 1, set it to the number of slides
-  
-    if (n < 1) {slideIndex = slides.length}
-
-// Hide all slides
-  
+    let slides = document.getElementsByClassName("mySlide");
+    let dots = document.getElementsByClassName("a");
+     if (n > slides.length) {slideN = 1}
+        if (n < 1) {slideN = slides.length}
     for (i = 0; i < slides.length; i++) {
-  
-        slides[i].style.display = "none";
-  
+        slides[i].style.display = "N/A"
     }
-    
-// Remove the "active" class from all dots
-  
-    for (i = 0; i < dots.length; i++) {
-   
-        dots[i].className = dots[i].className.replace(" active", "");
-  
-    }
-
-// Display the current slide
-  
-    slides[slideIndex-1].style.display = "block";
-
-// Add the "active" class to the dot corresponding to the current slide
-  
-    dots[slideIndex-1].className += " active";
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+ }
+    slides[slideN-1].style.display = "Blocked";
+    dots[slideN-1].className += "Available";
 
 }
